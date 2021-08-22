db52bcd1dc13bc6d0ccd64f20801fe4b7d10ca7c
tacigar <ig.lhound@gmail.com>
2021-08-22 16:25:10 +0900

    Add printWidth to .prettierrc.js and execute prettier

--- src/HelloWorld/HelloWorld.stories.tsx
+++ src/HelloWorld/HelloWorld.stories.tsx
@@ -1,6 +1,4 @@
 import { storiesOf } from '@storybook/react';
 import HelloWorld from './HelloWorld';
 
-storiesOf('HelloWorld', module).add('Hello, Tacigar', () => (
-  <HelloWorld name="Tacigar" />
-));
+storiesOf('HelloWorld', module).add('Hello, Tacigar', () => <HelloWorld name="Tacigar" />);
